614d2abba21a24805161e9bb39a13c94778fd36d
G.l.s.t.n.d <glistandirovka@gmail.com>
2025-03-11 13:16:22 +0300

    added other urls for docs

--- Backend/AccountsService/main.py
+++ Backend/AccountsService/main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
 
 from database.database import init_db
+from routes.docs import add_docs
 
 app = FastAPI()
+add_docs(app)
+
 
 @app.on_event("startup")
 async def on_startup():
     await init_db()
+
 
 @app.get("/")
 async def root():
