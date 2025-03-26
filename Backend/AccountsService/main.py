e3a67c30bef99a5bab029f657cabb6858b999a00
G.l.s.t.n.d <glistandirovka@gmail.com>
2025-03-26 23:45:38 +0300

    added token exception handling

--- Backend/AccountsService/main.py
+++ Backend/AccountsService/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from database.database import init_db
+from exceptions import exceptions_handlers
 from routes.admins import admin_router
 from routes.docs import add_docs
 from routes.users import user_router
@@ -14,15 +15,21 @@ app = FastAPI(
     redoc_url=None
 )
 
+origins = [
+    "http://localhost:5173",
+    "https://localhost:5173",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 add_docs(app)
+exceptions_handlers(app)
 
 app.include_router(user_router, prefix="/users")
 app.include_router(admin_router, prefix="/admins")
