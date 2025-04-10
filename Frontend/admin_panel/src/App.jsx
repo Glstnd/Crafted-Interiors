ac9644e106f259311fba75b72037b2c61b218d18
G.l.s.t.n.d <glistandirovka@gmail.com>
2025-04-10 23:07:44 +0300

    clear admin panel project

--- Frontend/admin_panel/src/App.jsx
+++ Frontend/admin_panel/src/App.jsx
@@ -1,34 +1,12 @@
 import './App.css';
 import React from "react";
-import HomePage from "./pages/HomePage/HomePage";
 import {Routes, Route} from "react-router-dom";
-import Header from "./components/Header/Header.jsx";
-import AdminsPage from "./pages/AdminsPage/AdminsPage.jsx";
-import LoginPage from "./pages/LoginPage/LoginPage.jsx";
-import CatalogsPage from "./pages/CatalogsPage/CatalogsPage.jsx";
 
 
 function App() {
     return (
         <>
-            <Header/>
             <Routes>
-                <Route
-                    path="/"
-                    element={<HomePage/>}
-                />
-                <Route
-                    path="/admins"
-                    element={<AdminsPage/>}
-                />
-                <Route
-                    path="/login"
-                    element={<LoginPage/>}
-                />
-                <Route
-                    path="/catalogs"
-                    element={<CatalogsPage/>}
-                />
             </Routes>
         </>
     );
