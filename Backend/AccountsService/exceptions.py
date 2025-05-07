dd2a79da4a681c1bbf77e61329a7fa2a3ebf8b73
G.l.s.t.n.d <glistandirovka@gmail.com>
2025-05-07 19:12:23 +0300

    added logs for accounts_service

--- Backend/AccountsService/exceptions.py
+++ Backend/AccountsService/exceptions.py
@@ -6,10 +6,13 @@ from starlette import status
 from starlette.requests import Request
 from starlette.responses import JSONResponse
 
+from logs import logger
+
 
 def exceptions_handlers(app: FastAPI):
     @app.exception_handler(MissingTokenError)
     async def http_exception_handler(request: Request, exc: MissingTokenError):
+        logger.error(f"\n{request.method}\n{request.url}\n{exc}\n{request.headers}")
         return JSONResponse(
             {"error": "Admin not authorized"},
             status_code=status.HTTP_401_UNAUTHORIZED)
